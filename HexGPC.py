dcee53568a8db89c503adfde1731fb85926df2b1
Byt3signal <eaeeae645@gmail.com>
2024-12-08 09:52:39 -0800

    Update HexGPC.py
    Added Video Support😊

--- HexGPC.py
+++ HexGPC.py
@@ -1,8 +1,8 @@
 import os
-from hexgpc import converter
+from hexgpc import process_file
 
 def main():
-    file_path = input("Enter the path of the image: ").strip().strip('"').strip("'")
+    file_path = input("Enter the path of the file (image or video): ").strip().strip('"').strip("'")
     if not os.path.isfile(file_path):
         print("Error: File not found.")
         return
@@ -19,16 +19,20 @@ def main():
         print("Invalid choice.")
         return
 
+    max_frames_input = input("Enter maximum number of frames to process (default 20): ").strip()
+    max_frames = int(max_frames_input) if max_frames_input.isdigit() else 20
+
     try:
-        gpc_script = converter(file_path, invert=invert)
+        c_code = process_file(file_path, invert, max_frames)
     except Exception as e:
-        print(f"Error processing image: {e}")
+        print(f"Error processing file: {e}")
         return
 
     base = os.path.splitext(os.path.basename(file_path))[0]
     output_file = f"{base}.gpc"
     with open(output_file, "w") as file:
-        file.write(gpc_script)
+        file.write(c_code)
+
     print(f"Hexadecimal values have been saved to {output_file}.")
 
 if __name__ == "__main__":
